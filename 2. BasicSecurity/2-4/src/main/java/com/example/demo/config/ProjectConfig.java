9521d68e68c64491f4d3a168b11e4069eedcd68a
zzangjyj0818 <jyj010818@naver.com>
2024-01-25 01:36:52 +0900

    http basic security

--- 2. BasicSecurity/2-4/src/main/java/com/example/demo/config/ProjectConfig.java
+++ 2. BasicSecurity/2-4/src/main/java/com/example/demo/config/ProjectConfig.java
@@ -4,25 +4,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+// 권장하지 않는 접근방식
+// 보안처리가 너무 복잡해지기 때문
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        var user = User.withUsername("john")
+        auth.inMemoryAuthentication()
+                .withUser("john")
                 .password("12345")
                 .authorities("read")
-                .build();
-
-        userDetailsService.createUser(user);
-
-        auth.userDetailsService(userDetailsService)
+                .and()
                 .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 
