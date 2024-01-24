964cdf4283b89a1861664029387324d5d6c35675
zzangjyj0818 <jyj010818@naver.com>
2024-01-25 02:11:19 +0900

    refactoring

--- 2. BasicSecurity/2-5/src/main/java/com/example/demo/config/ProjectConfig.java
+++ 2. BasicSecurity/2-5/src/main/java/com/example/demo/config/ProjectConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo.config;
 
+import com.example.demo.security.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,24 +13,18 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
+
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
-
-        userDetailsService.createUser(user);
-
-        auth.userDetailsService(userDetailsService)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    protected void configure(AuthenticationManagerBuilder auth){
+        auth.authenticationProvider(customAuthenticationProvider);
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception{
         http.httpBasic();
         http.authorizeRequests().anyRequest().authenticated();
     }
+
 }
