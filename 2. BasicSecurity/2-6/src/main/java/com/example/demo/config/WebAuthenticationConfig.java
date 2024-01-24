dea006c03a17a8360e15d8a2ef75bb5283d4e948
zzangjyj0818 <jyj010818@naver.com>
2024-01-25 02:17:30 +0900

    seperate the class

---
+++ 2. BasicSecurity/2-6/src/main/java/com/example/demo/config/WebAuthenticationConfig.java
@@ -0,0 +1,14 @@
+package com.example.demo.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebAuthenticationConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
