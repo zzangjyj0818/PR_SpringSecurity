afd7371615547265dfb658f87204de8456e48140
zzangjyj0818 <jyj010818@naver.com>
2024-01-25 02:07:26 +0900

    implement AuthenticationProvider

---
+++ 2. BasicSecurity/2-5/src/main/java/com/example/demo/security/CustomAuthenticationProvider.java
@@ -0,0 +1,30 @@
+package com.example.demo.security;
+
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+
+        if("john".equals(username) && "12345".equals(password)){
+            return new UsernamePasswordAuthenticationToken(username, password, Arrays.asList());
+        } else {
+            throw new AuthenticationCredentialsNotFoundException("Error in authentication");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(aClass);
+    }
+}
