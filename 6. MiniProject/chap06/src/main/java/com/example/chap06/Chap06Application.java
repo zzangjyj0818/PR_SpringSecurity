3f066a38ddb096cb00ae9168f838b283678a4cf1
zzangjyj0818 <jyj010818@naver.com>
2024-01-29 01:02:52 +0900

    new project

---
+++ 6. MiniProject/chap06/src/main/java/com/example/chap06/Chap06Application.java
@@ -0,0 +1,13 @@
+package com.example.chap06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap06Application.class, args);
+	}
+
+}
---
+++ 6. MiniProject/chap06/src/test/java/com/example/chap06/Chap06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chap06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
