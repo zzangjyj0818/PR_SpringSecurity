318a1a1afbb9835684a6f075612001e333533b59
zzangjyj0818 <jyj010818@naver.com>
2024-02-11 22:58:31 +0900

    Setting DTO

--- react-context-jwt-security/server/src/main/java/com/yeonjae/server/dto/UserAuth.java
+++ react-context-jwt-security/server/src/main/java/com/yeonjae/server/dto/UserAuth.java
@@ -1,4 +1,14 @@
 package com.yeonjae.server.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserAuth {
+    private int authNo;
+    private String userId;
+    private String auth;
 }
--- react-context-jwt-security/server/src/main/java/com/yeonjae/server/dto/Users.java
+++ react-context-jwt-security/server/src/main/java/com/yeonjae/server/dto/Users.java
@@ -1,4 +1,26 @@
 package com.yeonjae.server.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users {
+    private int no;
+    private String userId;
+    private String userPw;
+    private String userPwCheck;
+    private String name;
+    private String email;
+    private Date regDate;
+    private Date updDate;
+    private int enabled;
+
+    List<UserAuth> authList;
+
 }
