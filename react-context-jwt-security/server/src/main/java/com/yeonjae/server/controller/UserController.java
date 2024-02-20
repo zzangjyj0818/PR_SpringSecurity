654398b53df30591739ddb9bc2e987411c9a5057
zzangjyj0818 <jyj010818@naver.com>
2024-02-20 10:14:17 +0900

    commit

--- react-context-jwt-security/server/src/main/java/com/yeonjae/server/controller/UserController.java
+++ react-context-jwt-security/server/src/main/java/com/yeonjae/server/controller/UserController.java
@@ -8,9 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -19,7 +17,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/info")
+    @PostMapping("/info")
     public ResponseEntity<?> userInfo(@AuthenticationPrincipal CustomUser customUser) {
         log.info("customUser : " + customUser);
 
